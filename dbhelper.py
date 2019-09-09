1ec8067c1e72d002de9035bc09ddf09f09ea86c4
Krishna Pal Tiwari <krishnap152@gmail.com>
2019-09-09 12:36:44 +0530

    updated dbhelper

--- dbhelper.py
+++ dbhelper.py
@@ -21,8 +21,7 @@ class DBHelper():
     def add_input(self,data):
         connection = self.connect()
         try:
-            query = "INSERT INTO crimes (description) VALUES
-            ('{}');".format(data)
+            query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
             with  connection.cursor() as cursor:
                 cursor.execute(query)
                 connection.commit()
